1d663b29df0fecc685d79a7cec5f02bc4e8d859d
Ibrahima <ibrahima@ohlone.fr>
2024-09-18 16:39:50 +0200

    V1

--- next.config.js
+++
@@ -1,6 +0,0 @@
-// next.config.js
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-export default nextConfig;
--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-// postcss.config.js
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
-
-export default config;
